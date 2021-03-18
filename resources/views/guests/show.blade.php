a401eee072e232e9235acead464e8c27c708d72d
Emilio <emilio.furnari87@gmail.com>
2021-03-18 17:44:34 +0100

    layout init

--- resources/views/guests/show.blade.php
+++ resources/views/guests/show.blade.php
@@ -1,35 +1,39 @@
 @extends('layouts.main')
 @section('content')
-<div class="jumbo">
-    <div class="restaurant">
-        <h1>Nome ristorante</h1>
-    </div>
+<span id="emilio">
+    <div class="container">
+        <div class="jumbo">
+            <div class="restaurant">
+                <h1>{{$restaurant->name}}</h1>
+            </div>
 
-</div>
-<div class="main-menu">
-    <div class="cart">
-        <h2>Carrello</h2>
-        <div v-for='plate in cart'>
-            <span>@{{ plate.name }}</span>
-            <span> @{{ plate.price*plate.counter }}</span>
-            <span>@{{ plate.counter }}</span>
         </div>
-        <h4>@{{ totalPrice }}</h4>
+        <div class="main-menu">
+            <div class="cart">
+                <h2>Carrello</h2>
+                <div v-for='plate in cart'>
+                    <span class="plateName">@{{ plate.name }}</span>
 
+                    <span class="platePrice"> @{{ plate.price*plate.counter }}</span>
+                    <span class="plateCounter">@{{ plate.counter }}</span>
+                </div>
+                <h4>@{{ totalPrice }}</h4>
+
+            </div>
+
+            <section>
+                <h1>I Nostri Piatti</h1>
+                <plate-component
+                    v-for="plate in plates"
+                    :name="plate.name"
+                    :id="plate.id"
+                    :price="plate.price"
+                    :key="plate.id">
+                </plate-component>
+            </section>
+        </div>
     </div>
-
-    <section>
-        <h1>I piatti</h1>
-        <plate-component
-            v-for="plate in plates"
-            :name="plate.name"
-            :id="plate.id"
-            :price="plate.price"
-            :key="plate.id">
-        </plate-component>
-    </section>
-</div>
-
+</span>
 @endsection
 <script>
     var id = {!! json_encode($restaurant->id) !!};
