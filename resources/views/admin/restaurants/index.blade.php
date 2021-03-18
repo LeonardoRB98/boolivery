f5f18ac5a6887dbd2fea3488fff3a1a69188ce9a
Andrea Serboli <andrea.serboli12@gmail.com>
2021-03-18 17:41:52 +0100

    card index ristoranti admin

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,53 +1,46 @@
 @extends('layouts.main')
 
 @section('content')
+<div id="andrea">
     <div class="container">
         @if (session('message'))
         <div class="alert alert-success">
         {{ session('message') }}
         </div>
         @endif
-        <div class="clearfix mb-4">
-            <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary float-right">Crea Ristorante</a>
-        </div>
 
-    <h1>Tutti i Ristoranti</h1>
+    <h1>I tuoi Ristoranti</h1>
 
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Indirizzo</th>
-                    <th>Telefono</th>
-                    <th>Descrizione</th>
-                    <th>
-                       Foto
-                    </th>
-                    <th>foto back</th>
-                    <th colspan="3"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($restaurants as $restaurant)
-                <tr>
+    <div class="clearfix mb-4">
+        <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary float-right">Crea Ristorante</a>
+    </div>
+
+    <div class="container_card">
+        @foreach ($restaurants as $restaurant)
+        <a href="{{route('admin.restaurants.show', $restaurant)}}">
+            <div class="card">
+                <div class="image-box">
+                    @if (!is_null($restaurant->photo))
+                    <img class="img-fluid" src="{{ asset('storage/'. $restaurant->photo) }}" alt="{{ $restaurant->name }}">
+                    @else
+                        <img src="{{ asset('image/download.png') }}" alt="{{ $restaurant->name }}">
+                    @endif
+                    
+                </div>
+                
+                <h3>{{ $restaurant->name }}</h3>
+                <p>{{ $restaurant->description }}</p>
+            </div>
+        </a> 
+                {{-- <tr>
                     <td> {{ $restaurant->id }}</td>
-                    <td> <a href="{{route('admin.restaurants.show', $restaurant)}}">{{ $restaurant->name }}</a> </td>
+                    <td> </td>
                     <td>{{ $restaurant->email }}</td>
                     <td>{{ $restaurant->address }}</td>
                     <td>{{ $restaurant->phone }}</td>
                     <td>{{ $restaurant->description }}</td>
 
-                        @if (!is_null($restaurant->photo))
-                            <td>
-                                <img style="width: 200px" class="img-fluid" src="{{ asset('storage/'. $restaurant->photo) }}" alt="{{ $restaurant->name }}">
-                            </td>
-                        @else
-                            <td>
-                                <img style="width: 200px" class="img-fluid" src="{{ asset('image/download.png') }}" alt="{{ $restaurant->name }}">
-                            </td>
-                        @endif
+                       
                         @if (!is_null($restaurant->photo_jumbo))
                             <td>
                                 <img style="width: 200px" class="img-fluid" src="{{ asset('storage/'. $restaurant->photo_jumbo) }}" alt="{{ $restaurant->name }}">
@@ -67,10 +60,12 @@
                             <input type="submit" value="Elimina">
                         </form>
                     </td>
-                </tr>
+                </tr> --}}
                 @endforeach
-            </tbody>
-        </table>
     </div>
+
+
+</div>
+    
 
 @endsection
