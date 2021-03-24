54c10775d5da5d8096ef7933f64f8b7070ba36c6
Emilio <emilio.furnari87@gmail.com>
2021-03-24 16:01:11 +0100

    checkout layout completo

--- resources/views/guests/checkout.blade.php
+++ resources/views/guests/checkout.blade.php
@@ -10,8 +10,11 @@
                         <h2>Controlla Il Tuo Ordine</h2>
                         <i class="fas fa-cart-arrow-down"></i>
                         <div class="order_plate" v-for='plate in cart'>
-                            <ul>
-                                <li class="plateCounter">
+                            <ul class="shadow">
+                                <li class="plateName">@{{ plate . name }}</li>
+                                <li class="plateCounter">X @{{plate.counter}}</li>
+                                <li class="platePrice">@{{ plate.price * plate.counter }} €</li>
+                                <li class="plateAddRemove">
                                     {{-- <i class="fas fa-arrow-left left"></i> --}}
                                     <i v-on:click="add(plate.id, plate.counter, plate.price, plate.name)"
                                         class="fas fa-plus-square" id="plus_checkout"></i>
@@ -19,12 +22,10 @@
                                         class="fas fa-minus-square" id="minus_checkout"></i>
                                     {{-- <i class="fas fa-arrow-right right"></i> --}}
                                 </li>
-                                <li class="plateName">@{{ plate . name }}</li>
-                                <li class="platePrice">@{{ plate . price * plate . counter }} €</li>
                             </ul>
                         </div>
                         <hr>
-                        <h4>Totale: <br> @{{ totalPrice }} €</h4>
+                        <h4 class="shadow">Totale: <br> @{{ totalPrice }} €</h4>
                     </div>
                 </div>
             </section>
@@ -37,44 +38,41 @@
                         {{ $error }}
                     @endforeach
                 @endif
-               <section class="checkout_right">
-            <form method="post" id="payment-form" action="{{ route('payment')}}">
-                @csrf
-                @method("post")
-                <div class="row">
-                    <input type="text" name="name" class="form-control" placeholder="Inserisci il tuo nome" value="Emilio">
-                </div>
-                <div class="row">
-                    <input type="text" name="surname" class="form-control" placeholder="Inserisci il tuo cognome" value="Furnari">
-                </div>
-                <div class="row">
-                    <input type="email" name="email" class="form-control" id="email" placeholder="Inserisci la tua email" value="Furnari@mail.com">
-                </div>
-                <div class="row">
-                    <input type="address" name="address" class="form-control" id="address" value="Via delle cose" placeholder="Inserisci la tua email">
-                </div>
-                <div class="form-group">
-                    <select style="display:none;"  class="form-control" name="plates[]" multiple>
-                            <option v-for="plate in cart" :value="plate.id" selected>@{{plate.id}}</option>
-                    </select>
-            </div>
-                <section>
-                    <label for="amount">
-                        <span class="input-label">Totale da pagare: @{{ totalPrice }}  € </span>
-                        <div class="input-wrapper amount-wrapper amount">
-                            <input id="total" name="total" type="tel" min="1" placeholder="Totale" :value="totalPrice">
+               <section class="checkout_braintree shadow">
+                     <form method="post" id="payment-form" action="{{ route('payment')}}">
+                        @csrf
+                        @method("post")
+                        <div class="row shadow mb-2">
+                            <input type="text" name="name" class="form-control" placeholder="Inserisci il tuo nome" value="" required>
                         </div>
-                    </label>
-
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin">
+                        <div class="row shadow mb-2">
+                            <input type="text" name="surname" class="form-control" placeholder="Inserisci il tuo cognome" value="" required>
                         </div>
-                    </div>
+                        <div class="row shadow mb-2">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Inserisci il tua mail" value="" required>
+                        </div>
+                        <div class="row shadow mb-2">
+                            <input type="address" name="address" class="form-control" id="address" value="" placeholder="Inserisci il tuo indirizzo" required>
+                        </div>
+                        <div class="form-group">
+                            <select style="display:none;"  class="form-control" name="plates[]" multiple>
+                                    <option v-for="plate in cart" :value="plate.id" selected>@{{plate.id}}</option>
+                            </select>
+                        </div>
+                        <label for="amount">
+                            <div class="input-wrapper amount-wrapper amount">
+                                <input id="total" name="total" type="tel" min="1" placeholder="Totale" :value="totalPrice">
+                            </div>
+                        </label>
+                        <div class="bt-drop-in-wrapper">
+                            <div id="bt-dropin">
+                            </div>
+                        </div>
+                        <input id="nonce" name="payment_method_nonce" type="hidden"/>
+                        <button class="btn btn-success pay" type="submit"><span>Paga</span></button>
+                    </form>
                 </section>
-
-                <input id="nonce" name="payment_method_nonce" type="hidden"/>
-                <button class="btn btn-success pay" type="submit"><span>Paga</span></button>
-            </form>
+            </section>
            @section('braintree')
                 <script src="https://js.braintreegateway.com/web/dropin/1.27.0/js/dropin.min.js"></script>
                 <script>
@@ -115,7 +113,7 @@
                     });
                 </script>
             </section>
-        @endsection
-    </div>
-</span>
+            @endsection
+        </div>
+    </span>
 @endsection
