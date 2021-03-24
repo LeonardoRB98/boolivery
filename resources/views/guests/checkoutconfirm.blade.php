e33c6c6535f6b3f069b747517c14c09518c5b450
Emilio <emilio.furnari87@gmail.com>
2021-03-24 17:42:40 +0100

    fix fireworks

--- resources/views/guests/checkoutconfirm.blade.php
+++ resources/views/guests/checkoutconfirm.blade.php
@@ -4,17 +4,17 @@
 @section('content')
 
     <div class="container-fluid central">
-        <div class="pyro">
+        {{-- <div class="pyro">
             <div class="before"></div>
             <div class="after"></div>
-        </div>
+        </div> --}}
         <div class="message shadow">
             <h1>Ordine effettuato con successo</h1>
         </div>
-         <div class="pyro">
+         {{-- <div class="pyro">
             <div class="before"></div>
             <div class="after"></div>
-        </div>
+        </div> --}}
     </div>
 
 
