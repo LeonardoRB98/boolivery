d43ae62d562a1092529924ca7136f1ec586f1c67
Emilio <emilio.furnari87@gmail.com>
2021-03-24 16:31:03 +0100

    confirm layout abbozzato

--- resources/views/guests/checkoutconfirm.blade.php
+++ resources/views/guests/checkoutconfirm.blade.php
@@ -1,1 +1,21 @@
-ciao
+@extends('layouts.main')
+
+
+@section('content')
+
+    <div class="container-fluid central">
+        <div class="pyro">
+            <div class="before"></div>
+            <div class="after"></div>
+        </div>
+        <div class="message shadow">
+            <h1>Ordine effettuato con successo</h1>
+        </div>
+         <div class="pyro">
+            <div class="before"></div>
+            <div class="after"></div>
+        </div>
+    </div>
+
+
+@endsection
