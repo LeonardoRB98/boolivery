96364f7313ccbac7f4aabe07530f94787fdd033a
Stephanie <stephanie.furlan94@gmail.com>
2021-03-26 10:23:11 +0100

    Login reset password msg

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,8 +5,9 @@
         <div class="row justify-content-center align-items-center wrap-box">
             <div class="col-md-6">
                 @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                    <div class="alert alert-success mb-5" role="alert">
+                        {{-- {{ session('status') }} --}}
+                        Mail inviata con successo!
                     </div>
                 @endif
 
@@ -16,7 +17,7 @@
                     <div class="form-group row">
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Inserisci email...">
                         @error('email')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="error-form" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
@@ -24,7 +25,7 @@
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
                             <button type="submit" class="blue-button">
-                                {{ __('Send Password Reset Link') }}
+                                Invia Recupero Password
                             </button>
                         </div>
                     </div>
