7fa9df89e0b833b70e2864a578da09d43f4a5a13
fortunato <sambataro1990@gmail.com>
2021-03-26 10:34:06 +0100

    aggiunto icona nel nome del sito

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Boolivery</title>
+    <link rel="shortcut icon" href="{{ asset('image/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('image/favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
